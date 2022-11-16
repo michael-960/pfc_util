d90a806bc3fd894b48f9683bf3456ef7ec7a5ab9
michael-960 <mike.wang96029@gapp.nthu.edu.tw>
2022-11-16 13:00:20 +0800

    docs mod

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,6 +6,9 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
+import sys, os
+sys.path.insert(0, os.path.abspath('../../src/'))
+
 project = 'pfc-util'
 copyright = '2022, MichaelWang'
 author = 'MichaelWang'
