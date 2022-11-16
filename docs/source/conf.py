6068f8687e29f67bffdc95ed15be083662fd66c6
michael-960 <mike.wang96029@gapp.nthu.edu.tw>
2022-11-16 15:52:06 +0800

    doc5

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,8 +18,8 @@ release = '0.4.10'
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary'
+    'sphinx.ext.autodoc'
+    # 'sphinx.ext.autosummary'
 ]
 
 templates_path = ['_templates']
